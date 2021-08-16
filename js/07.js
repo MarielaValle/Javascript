cbdc6ded08e72b50867f3e31f482dc80fa8c1a75
Mariela Vallecillo <mvalletelt@gmail.com>
2021-08-16 12:19:34 -0300

    orden de operaciones y concatenación strings

---
+++ js/07.js
@@ -0,0 +1,32 @@
+//Orden de operaciones
+
+let resultado;
+
+resultado= (20+30) * 2,
+
+resultado=(100+200+300)*0.2; //porcentaje de descuentno
+
+resultado=(100+400+500)*1.21; //agregando el 21% IVA
+
+console.log(resultado)
+
+
+//Incrementos
+
+let puntaje=10;
+
+puntaje++;
+console.log(puntaje)
+
+//Otra opción
+
+let puntajeB=10;
+
+console.log(++puntajeB)
+
+//Otra opción
+
+let puntajeC=10;
+puntajeC +=10
+
+console.log(puntajeC)
---
+++ js/08.js
@@ -0,0 +1,22 @@
+//concatenar strings
+
+const nombre="Mariela";
+
+const email= "correo@correo.com";
+
+console.log(nombre);
+console.log(email);
+
+//Concatenación
+console.log(nombre + " "+email);
+
+console.log("Nombre cliente:"+nombre +" Email:"+email)
+
+console.log("Nombre cliente:",nombre ," Email:",email)
+
+
+
+//Template Strings
+
+console.log(`Nombre de Cliente: ${nombre} Email: ${email}`);
+
