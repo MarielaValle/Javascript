fdfcc5e928125ee48615f481a75da0e34936bd10
Mariela Vallecillo <mvalletelt@gmail.com>
2021-10-02 12:23:55 -0300

    arrow functions

--- js/21.js
+++ js/21.js
@@ -18,3 +18,66 @@ const aprendiendo = (tecnologia) => { //parámetro, cuando hay un solo parámtro
 }// cuando solo es una lína, las llaves son opcionales
 
 aprendiendo("Javascript")//argumento
+
+
+//---------------------------------------------
+
+const meses = ["enero", "febrero", "marzo","abril"]
+
+const carrito = [
+
+{nombre:"monitor", precio: 200},
+{nombre: "tablet", precio: 100}
+]
+
+// forEach
+
+meses.forEach (mes=>console.log(mes))
+
+meses.forEach (mes=>{
+
+    if(mes=="marzo"){
+
+        console.log("marzo si existe")
+    }
+
+})
+
+const resultado = meses.includes("marzo");
+console.log (resultado)
+
+//some ideal para arrays de objetos
+
+resultado2 = carrito.some ( producto=>  producto.precio == 100)
+
+console.log (resultado2)
+
+//--------------Forma corta con arrow function---------------
+
+resultado3 =carrito.some(producto=> producto.nombre =="monitor")
+
+console.log(resultado3);
+
+//Reduce
+
+resultado4 = carrito.reduce(function(total, producto){
+
+    return total + producto.precio
+},0)
+
+console.log(resultado4)
+
+resultado5 = carrito.reduce((total,producto) =>total + producto.precio,0);
+
+console.log(resultado5)
+
+//filter
+
+resultado6 = carrito.filter(function(producto){
+    return producto.precio >100
+})
+console.log(resultado6)
+
+resultado7 = carrito.filter ((producto)=>producto.precio>100)
+
+console.log(resultado7)
