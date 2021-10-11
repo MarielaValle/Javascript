b37dbe7f99a29378c92b13d7270646e85dbaac17
Mariela Vallecillo <mvalletelt@gmail.com>
2021-10-11 16:31:48 -0300

    prototype

--- js/27.js
+++ js/27.js
@@ -22,9 +22,46 @@ this.precio=precio;
 
 }
 
+function formatearProducto(producto){
+
+    return `El producto ${producto.nombre} tiene un precio de : ${producto.precio}$`
+}
+
 const producto2 = new Producto("Monitor",800);
 
 const producto3 = new Producto("Notebook", 1000);
 
+
+
+
 console.log(producto2);
 console.log(producto3);
+
+console.log(formatearProducto(producto2))
+
+
+
+//prototype nos va a permitir crear funciones que solo se utilitzan en un objeto determinaddo
+
+const cliente = {
+
+    nombre:"Maria",
+    apellido:"Valle"
+}
+
+function Cliente(nombre, apellido){
+    this.nombre=nombre;
+    this.apellido=apellido;
+    
+    }
+
+Cliente.prototype.formatearCliente = function(){
+
+    return `El nombre del cliente es ${this.nombre} y el apellido ${this.apellido}`
+}
+
+const cliente1 = new Cliente("Julio","Boca");
+
+console.log(cliente1);
+
+console.log(cliente1.formatearCliente())
