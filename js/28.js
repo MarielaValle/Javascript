bf829a63446623089b1c0bf3c02b5f4fe10108c4
Mariela Vallecillo <mvalletelt@gmail.com>
2021-10-12 09:51:01 -0300

    Herencia

--- js/28.js
+++ js/28.js
@@ -12,18 +12,47 @@ this.precio=precio;
 formatearProducto(){
     return `El Producto ${this.nombre} tiene un precio de : ${this.precio}$`
 }
+formatearPrecio(){
+
+    return `El precio del producto ${this.nombre} es de ${this.precio}$`
+}
+obtenerPrecio(){
+    console.log(this.precio)
+}
 
 }
 
 const producto1 = new Producto("Monitor", 800);
 
-console.log(producto1)
+console.log(producto1);
 
-console.log(producto1.formatearProducto())
+console.log(producto1.formatearProducto());
 
-
-
-
+console.log(producto1.formatearPrecio());
 
 
+//herencia
 
+class Libro extends Producto{
+
+constructor (nombre, precio, isbn){
+    super(nombre,precio);
+    this.isbn=isbn;
+}
+formatearPrecio(){
+
+    return `${super.formatearPrecio()} y su isbn es ${this.isbn}`
+}
+obtenerPrecio(){
+    super.obtenerPrecio();
+   
+}
+}
+
+const libro1 = new Libro("Javascript la Revolución", 900, 99999)
+
+console.log(libro1);
+console.log(libro1.formatearPrecio());
+
+
+console.log(libro1.obtenerPrecio());
